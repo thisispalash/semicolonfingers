c8353bac6dbd60ee3aa075b56bfba407f7db47fe
isthispalash <thisispalash@kdio.xyz>
2025-02-28 07:42:13 -0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/arms-400.png" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
