44d9b83a6db474b572e700d1bd1b9362d03eeaf8
Palash A. <pa334@cornell.edu>
2025-03-03 00:19:09 +0530

    Sign up modal (#5)
    * add supabase
    
    * create modal
    
    * create route

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { supabaseAdmin } from '@/lib/supabase';
+
+export async function POST(request: NextRequest) {
+  const { email, name, interested_in } = await request.json();
+  console.log(email, name, interested_in);
+
+  const { error } = await supabaseAdmin
+    .from('signup')
+    .insert({ email, name, interested_in });
+  
+  if (error) {
+    return NextResponse.json({ message: 'Signup failed' }, { status: 500 });
+  }
+  return NextResponse.json({ message: 'Signup successful' });
+}
